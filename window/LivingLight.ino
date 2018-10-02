8c0bf4c7314ee8b0175d56df33577955478ef71f
Tim-AQ <tim.aq@protonmail.com>
2018-10-02 03:18:07 +0200

    Living Light

---
+++ window/LivingLight.ino
@@ -0,0 +1,85 @@
+#define MAX_SPEED 2
+#define MAX_GRADIANT 12
+
+DEFINE_GRADIENT_PALETTE( whiteglow_gp ) {
+  0,    255,  225,  190,
+  64,  255,  0,  190,
+  128, 0, 225,  190,
+  192, 255,  225,  0,
+  255,  255,  225,  190
+};
+DEFINE_GRADIENT_PALETTE( rainbowglow_gp ) {
+  0,    255,  225,  190,
+  64,  255,  25,  19,
+  128, 30, 225,  19,
+  192, 30,  25,  190,
+  255,  255,  225,  190
+};
+DEFINE_GRADIENT_PALETTE( redglow_gp ) {
+  0,    0,  0,  0,
+  48,  155,  100,  0,
+  80,  55,  10,  0,
+  128, 0,  0,  0,
+  176, 155,  55,  0,
+  208, 105,  75,  0,
+  255,  0,  0,  0
+};
+DEFINE_GRADIENT_PALETTE( yellowglow_gp ) {
+  0,    0,  0,  0,
+  64,  155,  100,  25,
+  128, 0, 0,  0,
+  192, 155,  75,  0,
+  255,  0,  0,  0
+};
+CRGBPalette16 yellowglow = yellowglow_gp;
+CRGBPalette16 redglow = redglow_gp;
+CRGBPalette16 whiteglow = whiteglow_gp;
+CRGBPalette16 rainbowglow = rainbowglow_gp;
+
+
+uint8_t colourIndex[NUM_LEDS];
+int8_t  colourSpeed[NUM_LEDS];
+
+void livingLight() {
+  randomiseSpeed();
+  incrementColourIndex();
+
+  for(uint8_t i=0; i<NUM_LEDS; i++) {
+    leds[i] = ColorFromPalette(redglow, colourIndex[i], 255, LINEARBLEND);
+  }
+}
+
+
+void randomiseSpeed() {
+  if(random8()<64) {
+      colourSpeed[0] = constrain( colourSpeed[0] + random(-1,2), -MAX_SPEED, MAX_SPEED);
+  }
+  if(random8()<64) {
+    colourSpeed[NUM_LEDS-1] = constrain( colourSpeed[NUM_LEDS-1] + random(-1,2), -MAX_SPEED, MAX_SPEED);
+  }
+  for(uint8_t i=1; i<NUM_LEDS-1; i++) {
+    int deviation = colourIndex[i-1] + colourIndex[i+1] - colourIndex[i]*2;
+    if( deviation > MAX_GRADIANT ) {
+      colourSpeed[i] = constrain( colourSpeed[i]+random(0,3), -MAX_SPEED, MAX_SPEED);
+    } else if( deviation < -MAX_GRADIANT ) {
+      colourSpeed[i] = constrain( colourSpeed[i]+random(-2,1), -MAX_SPEED, MAX_SPEED);
+    } else if(random8()<64) {
+      colourSpeed[i] = constrain( colourSpeed[i] + random(-1,2), -MAX_SPEED, MAX_SPEED);
+    }
+  }
+}
+
+void incrementColourIndex() {
+  colourIndex[0] += colourSpeed[0];
+  for(uint8_t i=1; i<NUM_LEDS; i++) {
+    colourIndex[i] = colourIndex[i] + colourSpeed[i];
+  }
+}
+
+
+void resetLivingLightArrays() {
+  for(uint8_t i=0; i<NUM_LEDS; i++) {
+    colourIndex[i] = colourSpeed[i] = 0;
+  }
+}
+
--- window/window.ino
+++ window/window.ino
@@ -3,57 +3,47 @@
 FASTLED_USING_NAMESPACE
 
 #define DATA_PIN    6
-#define LED_TYPE    WS2812B
-#define COLOR_ORDER GRB
 #define NUM_LEDS    114
 CRGB leds[NUM_LEDS];
 
 #define BRIGHTNESS         196
-#define FRAMES_PER_SECOND  120
+#define FRAMES_PER_SECOND  60
 
 void setup() {
   Serial.begin(115200);
-  FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
   
   // set master brightness control
   FastLED.setBrightness(BRIGHTNESS);
-  FastLED.setTemperature( Candle );
+  //FastLED.setTemperature( Candle );
   Serial.println( F("Window") );
 }
 
 
 // List of patterns to cycle through.  Each is defined as a separate function below.
 typedef void (*SimplePatternList[])();
-SimplePatternList gPatterns = { rainbow };
+SimplePatternList gPatterns = { rainbow, livingLight };
 
-uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
+uint8_t gCurrentPatternNumber = 1; // Index number of which pattern is current
 uint8_t gHue = 0; // rotating "base color" used by many of the patterns
   
-void loop()
-{
+void loop() {
   // Call the current pattern function once, updating the 'leds' array
   gPatterns[gCurrentPatternNumber]();
-
-  // send the 'leds' array out to the actual LED strip
   FastLED.show();
-  // insert a delay to keep the framerate modest
   FastLED.delay(1000/FRAMES_PER_SECOND); 
-
-  // do some periodic updates
+  
   EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
-  //EVERY_N_SECONDS( 10 ) { nextPattern(); } // change patterns periodically
 }
 
 #define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
 
-void nextPattern()
-{
+void nextPattern() {
   // add one to the current pattern number, and wrap around at the end
   gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE( gPatterns);
 }
 
-void rainbow() 
-{
+void rainbow() {
   // FastLED's built-in rainbow generator
   fill_rainbow( leds, NUM_LEDS, gHue, 3);
 }
